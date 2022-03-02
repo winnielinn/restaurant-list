9b31eecc8cc44abba36a2c6025d0933603096125
Winnie <yaysa903@gmail.com>
2022-03-03 00:14:36 +0800

    feat: add login & register pages

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurans')
+const users = require('./modules/users')
 
 router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', restaurants)
 
 module.exports = router
---
+++ routes/modules/users.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  
+})
+
+module.exports = router
